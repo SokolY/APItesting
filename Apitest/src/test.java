f81eddda3a55481161f40823624d53d4cf15f592
sokoly <yura.sokol@gmail.com>
2022-02-13 19:30:38 +0200

    Data from anoter file2

--- Apitest/src/test.java
+++ Apitest/src/test.java
@@ -1,4 +1,6 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -8,8 +10,13 @@ public class test {
 
 	public static void main(String[] args) {
 		RestAssured.baseURI= "https://rahulshettyacademy.com";
-		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json").
-		body(payload.AddAddress()).when().post("maps/api/place/add/json").then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP")).header("server", "Apache/2.4.19 (Ubuntu)");
+		String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json").
+		body(payload.AddAddress()).when().post("maps/api/place/add/json").then().assertThat().statusCode(200).body("scope", equalTo("APP")).
+		extract().response().asString();
+		System.out.println(response);
+		JsonPath jsresp = new JsonPath(response);
+		String placeId = jsresp.getString("place_id");
+		System.out.println(placeId);
 	}
 
 }
