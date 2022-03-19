0ba926de4864f230debbe745fb4cb2d9e857de9b
sokoly <yura.sokol@gmail.com>
2022-03-19 15:10:47 +0200

    jira responce verify

--- Apitest/src/JiraAPI.java
+++ Apitest/src/JiraAPI.java
@@ -2,6 +2,8 @@
 
 import java.io.File;
 
+import org.testng.Assert;
+
 import io.restassured.RestAssured;
 import io.restassured.filter.session.SessionFilter;
 import io.restassured.path.json.JsonPath;
@@ -26,29 +28,49 @@ public class JiraAPI {
 		System.out.println("Session cookes is " + cookesId);
 		
 		//Add comment
-		given().pathParam("id", "10006").log().all().header("Content-Type", "application/json").body("{\r\n"
-				+ "    \"body\": \"Commet to the issue from Eclipse.\",\r\n"
+		String addCommentResponse = given().pathParam("id", "10006").header("Content-Type", "application/json").body("{\r\n"
+				+ "    \"body\": \"Commet to the issue from Eclipse for verefication.\",\r\n"
 				+ "    \"visibility\": {\r\n"
 				+ "        \"type\": \"role\",\r\n"
 				+ "        \"value\": \"Administrators\"\r\n"
 				+ "    }\r\n"
 				+ "}").filter(session).
 		when().post("/rest/api/2/issue/{id}/comment").
-		then().log().all().assertThat().statusCode(201);
+		then().assertThat().statusCode(201).extract().response().asString();
+		
+		System.out.println("Add comment responce is: " + addCommentResponse);
+		JsonPath addComentRaps = new JsonPath(addCommentResponse);
+		int neededCommentId = addComentRaps.getInt("id");
+		System.out.println("Comment id is: " + neededCommentId);
 //		
-//		
+////		
 //		//Attach file 
 //		given().filter(session).header("X-Atlassian-Token", "no-check").pathParam("key", "10006").header("Content-Type", "multipart/form-data").
 //		multiPart("file", new File("D:\\FromPC\\DiscD\\Java\\APITesting\\Apitest\\src\\AddPlace.txt")).
 //		when().post("/rest/api/2/issue/{key}/attachments").
 //		then().log().all().assertThat().statusCode(200);
-//		
+		
 		//get Issue
 		String issueText = given().filter(session).pathParam("key", "10006").queryParam("fields", "comment").
 		when().get("/rest/api/2/issue/{key}").
 		then().extract().response().asString();
 		
-		System.out.println("Issue field is: " + issueText);
+		JsonPath issueComments = new JsonPath(issueText);	
+		
+		int commentCountSize = issueComments.getInt("fields.comment.comments.size()");
+		
+		System.out.println("Count of comments is the issue is: " +  commentCountSize);
+		String commentText = null;
+		for(int i = 0; i <commentCountSize; i++) {
+			int commentId = issueComments.getInt("fields.comment.comments["+i+"].id");
+			System.out.println(commentId);
+			if(commentId == neededCommentId) {
+				commentText = issueComments.getString("fields.comment.comments["+i+"].body");
+//				System.out.println(commentText);
+				
+			}
+		}
+		Assert.assertEquals(commentText, "Commet to the issue from Eclipse for verefication.");
 	}
 
 }
